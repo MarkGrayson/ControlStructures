2423ebe8a40f794fed522479559b1e1ae7ef3b87
MarkGrayson <mpal7487@740s-pl-193.local>
2016-11-29 13:48:55 -0700

    Fixed bugs

--- src/control/Controller/ControlController.java
+++ src/control/Controller/ControlController.java
@@ -1,5 +1,54 @@
 package control.Controller;
 
-public class ControlController {
+import java.util.Iterator;
+import java.util.TimerTask;
 
+public class ControlController 
+{
+	public static void start()
+	{
+	int x = 10;
+	
+	while( x < 11) {
+		System.out.print("value of x : " + x );
+		x++;
+		System.out.print("\n");
+    	}
+	int z = 111;
+	
+	while( z < 112)
+		System.out.print("value of z : " + z );
+		z++;
+		System.out.print("\n");
+	
+	
+	int y = 11;
+	
+	while( y < 12) {
+		System.out.print("value of y : " + y );
+		y++;
+		System.out.print("/n");
+	}
+
+	
+	
+	{
+	if(true)
+	{
+		System.out.println("Hi,How are you?");
+	}
+		if(true)
+		{
+			System.out.println("I don't care!");
+		}
+			if(true)
+			{
+				System.out.println("Banana Bread!!");
+			}
+			
+			
+		
+
+	}
+}
 }
--- src/control/Controller/Runner.java
+++ src/control/Controller/Runner.java
@@ -1,5 +1,12 @@
 package control.Controller;
 
-public class Runner {
+public class Runner 
+
+{
+	public static void main (String [] args)
+	{
+		ControlController controlController = new ControlController();
+		controlController.start();
+	}
 
 }
